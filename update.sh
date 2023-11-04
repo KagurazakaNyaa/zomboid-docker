a000a8e9a59aae2328be897461edd4439f572632
神楽坂·喵 <dev@kagurazakanyaa.com>
2023-11-04 14:53:28 +0800

    Update github action bot info

--- update.sh
+++ update.sh
@@ -6,8 +6,8 @@ echo "$version" > currentversion
 if [[ "$currentversion" == "$version" ]]; then
     exit
 fi
-git config user.name github-actions
-git config user.email github-actions@github.com
+git config user.name "github-actions[bot]"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git add currentversion
 git commit -a -m "Auto Update Project Zomboid to buildid: $version"
 git tag -f latest
